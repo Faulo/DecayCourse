0f9599c9e3bb5eed3b8958bb8ed47cbee0d6ba18
Daniel Schulz <faulolio@gmail.com>
2019-01-03 17:51:39 +0100

    player shadow!!

---
+++ DecayCourse/Assets/Scripts/ShadowBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowBehaviour : MonoBehaviour
+{
+    float SurfacePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SurfacePosition = 0.51f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = transform.parent.position.y - SurfacePosition;
+        transform.localPosition = new Vector3(0, 0, offset);
+    }
+}
