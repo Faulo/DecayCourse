28feb5bafa69d22d1d6bd5294b7cef1f8706d919
Daniel Schulz <faulolio@gmail.com>
2019-01-06 22:30:19 +0100

    removed useless state machine

--- DecayCourse/Assets/Scripts/StateMachine/ConditionDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using StateMachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace StateMachine {
-    public delegate bool ConditionDelegate(StateController controller);
-}
--- DecayCourse/Assets/Scripts/StateMachine/ICondition.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace StateMachine {
-    public interface ICondition {
-        bool Triggered(StateController controller);
-    }
-}
